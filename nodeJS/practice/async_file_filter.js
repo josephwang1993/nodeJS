95de637ba3431abbc6e48b37275981c77d7847b0
JosephWang <josephwang1993@gmail.com>
2015-09-01 00:22:20 -0400

    lesson 6

--- nodeJS/practice/async_file_filter.js
+++ nodeJS/practice/async_file_filter.js
@@ -2,6 +2,14 @@
  * Created by jwang on 8/31/2015.
  */
 
-require('./async_file_filter_module.js');
+var myModule = require('./async_file_filter_module.js');
+var dir = process.argv[2];
+var ext = process.argv[3];
 
-mymodule();
+myModule(dir, ext, function(err, list){
+	if(err)
+		return err;
+	list.forEach(function(file){
+		console.log(file);
+	})
+});
--- nodeJS/practice/async_file_filter_module.js
+++ nodeJS/practice/async_file_filter_module.js
@@ -5,12 +5,14 @@
 
 var path = require('path');
 var fs = require('fs');
-var ext = process.argv[2];
 
-var mymodule = fs.readdir(path, function callback(err, list) {
-    if (err) return err;
-    for (var i = 0; i < list.length; i++) {
-        if (path.extname(list[i]) == ext)
-            console.log(list[i])
-    }
-});
+module.exports = function (dir, ext, callback) {
+	fs.readdir(dir, function(err, list){
+		if(err)
+			return callback(err);
+		list =list.filter(function(file){
+			return path.extname(file) === '.' + ext;
+		});
+		callback(null, list);
+	})
+};
